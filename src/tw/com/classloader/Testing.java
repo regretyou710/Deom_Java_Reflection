eda2787a1469750985d7d1d344b8c3e47d2e112f
regretyou710 <regretyou710@gmail.com>
2020-07-25 22:43:23 +0800

    202007252243

---
+++ src/tw/com/classloader/Testing.java
@@ -0,0 +1,52 @@
+package tw.com.classloader;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Test;
+
+public class Testing {
+
+	@Test
+	public void test1() {
+		// 對於自定義類，使用系統類加載器進行加載
+		ClassLoader classLoader = Testing.class.getClassLoader();
+		System.out.println(classLoader);// AppClassLoader:系統類加載器
+
+		ClassLoader classLoader1 = classLoader.getParent();
+		System.out.println(classLoader1);// ExtClassLoader:擴展類加載器
+
+		// 引導類加載器主要負責加載java的核心類庫
+		ClassLoader classLoader2 = classLoader1.getParent();
+		System.out.println(classLoader2);// 引導類加載器獲取不到，null
+
+		ClassLoader classLoader3 = String.class.getClassLoader();
+		System.out.println(classLoader3);// 引導類加載器獲取不到，null
+	}
+
+	@Test
+	public void test2() throws Exception {
+		// 資源文件載入
+		//方式一:
+		//默認文件路徑在Project底下
+		Properties prop1 = new Properties();
+		FileInputStream fis = new FileInputStream("jdbc1.properties");
+		//FileInputStream fis = new FileInputStream("src/jdbc2.properties");
+		prop1.load(fis);
+		String user1 = prop1.getProperty("user");
+		String pwd1 = prop1.getProperty("password");
+		System.out.println("user:" + user1 + "\t pwd:" + pwd1);
+		
+		//方式二:使用ClassLoader，默認文件路徑在src(資源文件夾)底下
+		Properties prop2 = new Properties();
+		ClassLoader classLoader = Testing.class.getClassLoader();
+		InputStream is = classLoader.getResourceAsStream("jdbc2.properties");		
+		prop2.load(is);
+		String user2 = prop2.getProperty("user");
+		String pwd2 = prop2.getProperty("password");
+		System.out.println("user:" + user2 + "\t pwd:" + pwd2);
+		
+	}
+}
---
+++ src/tw/com/newinstance/Testing.java
@@ -0,0 +1,72 @@
+package tw.com.newinstance;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import tw.com.reflection.Person;
+
+public class Testing {
+
+	/**
+	 * 透過反射創建應對的運行時類的物件
+	 * 
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 */
+	@Test
+	public void test1() throws InstantiationException, IllegalAccessException {
+		Class clazz = Person.class;
+		/*
+		 * Person.class->運行時類 newInstance():調用此方法，創建對應的運行時類的物件。內部調用了運行時類的空參建構式
+		 * 若要此方法正常運行，要求
+		 * 1.運行時類須提供空參建構式 
+		 * 2.空參建構式的存取修飾詞須public
+		 * 
+		 * 在javabean中要求提供一個空參public建構式，原因: 
+		 * 1.便於透過反射，創建運行時類的物件
+		 * 2.便於子類繼承此運行類時，默認調用super()，保證父類有此建構式
+		 */
+		Person p1 = (Person) clazz.newInstance();
+		System.out.println(p1);
+	}
+
+	@Test
+	public void test2() {
+		/*
+		 * 透過隨機數體現反射機制的動態性 
+		 * 反射機制動態性:在編譯時無法確定要創造哪個對象，運行時才知道要創造哪一對象
+		 */
+		for (int i = 0; i < 10; i++) {
+			int option = new Random().nextInt(3);// 0~2
+			String classPath = "";
+			switch (option) {
+			case 0:
+				classPath = "java.util.Date";
+				break;
+			case 1:
+				classPath = "java.lang.Object";
+				break;
+			case 2:
+				classPath = "tw.com.reflection.Person";
+				break;
+			}
+			try {
+				Object obj = getInstance(classPath);
+				System.out.println(obj);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public Object getInstance(String classPath) throws Exception {
+		/*
+		 * 創建一個指定類的物件 
+		 * classPath:指定類的全類名
+		 */
+		Class clazz = Class.forName(classPath);
+		return clazz.newInstance();
+	}
+}
